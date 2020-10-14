f01566e1ff2833ca84ac476bff6cead8b8c1d4f4
SloganHD <support@serl.uk>
2020-10-14 10:37:48 +0100

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,21 +8,26 @@ pattern="*.scss"
 
 
 if [ ! -z "${CONFIG_PATH}" ]; then
+  echo "== ${CONFIG_PATH} =="
   configPath=$CONFIG_PATH
 fi
 if [ ! -z "${INDENT_SPACES}" ]; then
+  echo "== ${INDENT_SPACES} =="
   indent_spaces=$INDENT_SPACES
 fi
 if [ ! -z "${PATTERN}" ]; then
+  echo "== ${PATTERN} =="
   pattern=$PATTERN
 fi
 
 stylelint_path="node_modules/.bin/stylelint"
 if [ ! -e stylelint_path ]; then
+  echo "== stylelint_path =="
   yarn add stylelint stylelint-config-standard --silent
 fi
 
-if [ ! "$(echo ${configPath}.stylelintrc*)" != "${configPath}.stylelintrc*" ]; then
+if [ ! "$(echo ${configPath}.stylelintrc)" != "${configPath}.stylelintrc" ]; then
+  echo "== making own config! =="
   echo "{
   \"extends\": \"stylelint-config-standard\",
   \"rules\": {
@@ -32,8 +37,8 @@ if [ ! "$(echo ${configPath}.stylelintrc*)" != "${configPath}.stylelintrc*" ]; t
 fi
 
 echo "=================================="
-echo "$(echo ${configPath}.stylelintrc*)"
-echo "${configPath}.stylelintrc*"
+echo "$(echo ${configPath}.stylelintrc)"
+echo "${configPath}.stylelintrc"
 echo "${stylelint_path}" >> $GITHUB_PATH
 echo $pattern
 echo "=================================="
