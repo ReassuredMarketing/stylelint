dc1c23d4d0624b177ff4aeee4d4d053c6d19dc53
SloganHD <support@serl.uk>
2020-10-15 10:51:28 +0100

    update

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,12 @@
-FROM exelban/baseimage:node-latest
+FROM php:7.4-cli
 
-COPY LICENSE README.md /
+RUN apt-get update && apt-get -y install zip unzip
 
-COPY entrypoint.sh /entrypoint.sh
+RUN curl -sS https://getcomposer.org/installer | php -- \
+--install-dir=/usr/bin --filename=composer && chmod +x /usr/bin/composer
+
+RUN npm install --save-dev stylelint stylelint-config-standard
+
+COPY "entrypoint.sh" "/entrypoint.sh"
 RUN chmod +x /entrypoint.sh
-
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 pattern="./src/**/*.scss"
 
-if [ ! -z "${PATTERN}" ]; then
-  echo "== ${PATTERN} =="
-  pattern=$PATTERN
-fi
-
-stylelint_path="node_modules/.bin/stylelint"
-if [ ! -e stylelint_path ]; then
-  yarn add stylelint stylelint-config-standard --silent
-fi
-
 echo "=================================="
-echo "stylelint path ${stylelint_path}"
 echo "Scanning ${pattern}"
 echo "=================================="
 
-sh -c "$stylelint_path $pattern"
+npx stylelint "${pattern}"
